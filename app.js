6baa9a639533fb8039d112768792f9cbb5fe3f79
Norvzy <c.ratanaleelas@gmail.com>
2026-04-18 16:04:28 +0700

    chore: configure CORS to allow credentials and local frontend origin

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(helmet());
